ed1fca73476a24b813907e4b21231f7d0014f513
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-05 12:34:27 +0400

    Update ListSorter.java

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.stream().sorted(new ListComparator());
+        sourceList.stream().forEach(System.out::println);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+
+        Integer k = Integer.valueOf(a);
+        Integer z = Integer.valueOf(b);
+        if(k < 0){
+            k *=-1;
+        }
+        if (z < 0){
+            z *=-1;
+        }
+        System.out.println("printing "+(k-z));
+        return k - z;
     }
 }
