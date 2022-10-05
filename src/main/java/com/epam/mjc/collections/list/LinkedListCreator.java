592dcf4a1b1ec8e3bb730971bd1748a7a08c5e10
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-05 12:33:56 +0400

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> oddEvenLinkList = new LinkedList<>();
+        for (Integer k: sourceList) {
+            if (k%2 == 0){
+                oddEvenLinkList.addLast(k);
+            }else {
+                oddEvenLinkList.addFirst(k);
+            }
+        }
+        return oddEvenLinkList;
     }
 }
