28b858ce056bc9cc8a46c185d6b0f43c076dfce9
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-05 12:33:26 +0400

    Update ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> threeDelimeter = new ArrayList<>();
+        for (String k: sourceList) {
+            if ((sourceList.indexOf(k)+1)%3 == 0){
+                threeDelimeter.add(k);
+                threeDelimeter.add(k);
+            }
+        }
+        return threeDelimeter;
     }
 }
